d0ab63151d0a1eed29fcb86292ee1fb5894bc781
Marcin7373 <skwarek.marcin@mydit.ie>
2020-04-21 10:26:06 +0100

    Scene created, Traffic lights and car spawning

---
+++ GE2Test/Assets/Scripts/LightsSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsSpawn : MonoBehaviour
+{
+    public float radius = 9;
+    public int lightNum = 10;
+    private float angle = 0, height;
+    private Vector3[] waypoints;
+    public GameObject trafficLight, car;
+
+    void OnEnable()
+    {
+        height = transform.position.y - (trafficLight.transform.localScale.y / 2);
+        waypoints = new Vector3[lightNum];
+
+        for (int i = 0; i < lightNum; i++)
+        {
+            waypoints[i] = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad) * radius, height, Mathf.Sin(angle * Mathf.Deg2Rad) * radius);
+            angle += 360 / lightNum;
+        }
+
+        for (int i = 0; i < lightNum; i++)
+        {
+            trafficLight = Instantiate(trafficLight, waypoints[i], trafficLight.transform.rotation);
+        }
+
+        car = Instantiate(car, transform.position, car.transform.rotation);
+    }
+}
